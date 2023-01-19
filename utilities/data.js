cec8efd9bee9e9574761d72209c9df0583624acd
DunstWalmart GitHub <lakmjaya@gmail.com>
2023-01-19 00:26:29 -0500

    first

---
+++ utilities/data.js
@@ -0,0 +1,7 @@
+const todoData = [
+    {description: 'Eat Healthy', complete: false},
+    {description: 'Work Out', complete: true},
+    {description: 'Get Smart', complete: false}
+]
+
+module.exports = todoData;
